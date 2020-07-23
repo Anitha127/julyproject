329b15c9e9b071ead32e39c562543eca74a9f6f2
Anitha127 <anithapriya1996@gmail.com>
2020-07-22 18:33:35 -0700

    check for repository

---
+++ sample.py
@@ -0,0 +1,28 @@
+# def add(a,b):
+#     return a+b
+# sum = add(10,20)
+# print(sum)
+# c = lambda a: print('even') if a % 2 == 0 else print('odd')
+# print(c(10))
+# from functools import reduce
+#
+# a1 = [10,21,11,45,60]
+# print(list(map(lambda a: a*a, a1)))
+#res = 0
+# a1 = [10,21,11,45,60]
+# a2 = []
+# def sum():
+#     #global res
+#     for i in a1:
+#         #res = res + i
+#         a2.append(i*i)
+#
+# sum()
+# print(res)
+
+# def add(a,b):
+#     c = a + b
+#     return c
+# sum = add(10, 20)
+# print(sum)
+import os
